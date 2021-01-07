d29413e578c9d8645db4cf5ba7466512dc19b30d
RicoLy <192520199@qq.com>
2021-01-07 23:55:07 +0800

    -

--- leetcode/0008MyAtoi/myAtoi.go
+++ leetcode/0008MyAtoi/myAtoi.go
@@ -2,48 +2,55 @@ package _008MyAtoi
 
 import (
 	"math"
-	"strings"
 )
 
 func myAtoI(str string) int {
-	return convert(clean(str))
-}
-
-func convert(sign int, absStr string) int {
-	absNum := 0
-	for _, b := range absStr {
-		absNum = absNum * 10 + int(b - '0')
-		switch {
-		case sign == 1 && absNum > math.MaxInt32:
-			return math.MaxInt32
-		case sign == -1 && absNum*sign < math.MinInt32:
-			return math.MinInt32
+	if len(str) == 0 {
+		return 0
+	}
+	strippedStr := stripLeadingWhitespace(str)
+	sign := 1
+	result := 0
+	for i, char := range strippedStr {
+		if i == 0 {
+			if char == '-' {
+				sign = -1
+			} else if char == '+' {
+				sign = 1
+			} else if int(char - '0') >= 0 && int(char - '0') <= 9 {
+				result = result * 10 + int(char - '0')
+			} else {
+				return result
+			}
+		} else {
+			if int(char - '0') < 0 || int(char - '9') > 9 {
+				break
+			}
+			if sign == -1 && result * 10 + sign * int(char - '0') < math.MinInt32 {
+				return math.MinInt32
+			}
+			if sign == 1 && result * 10 + int(char - '0') > math.MaxInt32 {
+				return math.MaxInt32
+			}
+			result = result * 10 + sign * int(char - '0')
 		}
 	}
-	return sign * absNum
+	return result
 }
 
-func clean(s string) (sign int, abs string) {
-	s = strings.TrimSpace(s)
-	if s == "" {
-		return
-	}
-	switch s[0] {
-	case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
-		sign, abs = 1, s
-	case '+':
-		sign, abs = 1, s[1:]
-	case '-':
-		sign, abs = -1, s[1:]
-	default:
-		abs = ""
-		return
-	}
-	for i, b := range abs {
-		if b < '0' || '9' > b {
-			abs = abs[:i]
-			break
+
+func stripLeadingWhitespace(str string) string {
+	result := ""
+	isLeading := true
+	for _, char := range str {
+		if isLeading {
+			if char != ' ' {
+				isLeading = false
+				result = result + string(char)
+			}
+		} else {
+			result = result + string(char)
 		}
 	}
-	return
+	return result
 }
