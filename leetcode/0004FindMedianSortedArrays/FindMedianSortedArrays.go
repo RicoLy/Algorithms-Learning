df4fc4faebbe2d59fd056635a08cab2884ff9ae8
RicoLy <192520199@qq.com>
2021-01-07 23:47:59 +0800

    -

--- leetcode/0004FindMedianSortedArrays/FindMedianSortedArrays.go
+++ leetcode/0004FindMedianSortedArrays/FindMedianSortedArrays.go
@@ -1,41 +1,45 @@
 package _004FindMedianSortedArrays
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	// 假设 nums1 的长度小
 	if len(nums1) > len(nums2) {
 		return findMedianSortedArrays(nums2, nums1)
 	}
-	low, high, k, nums1Mid, nums2Mid := 0, len(nums1), (len(nums1) + len(nums2) + 1)>>2, 0, 0
-
+	low, high, k, nums1Mid, nums2Mid := 0, len(nums1), (len(nums1)+len(nums2)+1)>>1, 0, 0
 	for low <= high {
-		nums1Mid = low + (high-low) >>1
+		// nums1:  ……………… nums1[nums1Mid-1] | nums1[nums1Mid] ……………………
+		// nums2:  ……………… nums2[nums2Mid-1] | nums2[nums2Mid] ……………………
+		nums1Mid = low + (high-low)>>1 // 分界限右侧是 mid，分界线左侧是 mid - 1
 		nums2Mid = k - nums1Mid
-		if nums1Mid > 0 && nums1[nums1Mid-1] > nums2[nums2Mid] {
-			high = nums1Mid -1
-		} else if nums1Mid != len(nums1) && nums1[nums1Mid] < nums2[nums2Mid-1] {
-			low = nums2Mid + 1
+		if nums1Mid > 0 && nums1[nums1Mid-1] > nums2[nums2Mid] { // nums1 中的分界线划多了，要向左边移动
+			high = nums1Mid - 1
+		} else if nums1Mid != len(nums1) && nums1[nums1Mid] < nums2[nums2Mid-1] { // nums1 中的分界线划少了，要向右边移动
+			low = nums1Mid + 1
 		} else {
+			// 找到合适的划分了，需要输出最终结果了
+			// 分为奇数偶数 2 种情况
 			break
 		}
 	}
-	midleft, midRigh := 0, 0
+	midLeft, midRight := 0, 0
 	if nums1Mid == 0 {
-		midleft = nums2[nums2Mid]
+		midLeft = nums2[nums2Mid-1]
 	} else if nums2Mid == 0 {
-		midleft = nums1[nums1Mid-1]
+		midLeft = nums1[nums1Mid-1]
 	} else {
-		midleft = max(nums1[nums1Mid-1], nums2[nums2Mid -1])
+		midLeft = max(nums1[nums1Mid-1], nums2[nums2Mid-1])
 	}
 	if (len(nums1)+len(nums2))&1 == 1 {
-		return float64(midleft)
+		return float64(midLeft)
 	}
 	if nums1Mid == len(nums1) {
-		midRigh = nums2[nums2Mid]
+		midRight = nums2[nums2Mid]
 	} else if nums2Mid == len(nums2) {
-		midRigh = nums1[nums1Mid]
+		midRight = nums1[nums1Mid]
 	} else {
-		midRigh = min(nums1[nums1Mid], nums2[nums2Mid])
+		midRight = min(nums1[nums1Mid], nums2[nums2Mid])
 	}
-	return float64(midleft+midRigh) / 2
+	return float64(midLeft+midRight) / 2
 }
 
 
